bb7ab6d5f7f285ce5a8b8fc09cb712dc56fe200b
Aleksey-Smirnov <asm09@mail.ru>
2021-06-30 21:32:31 +0300

    Release

--- uwsgi/env/assay.py
+++ uwsgi/env/assay.py
@@ -25,50 +25,63 @@ class Assays():
         def index():
                self.session['id'] = secrets.token_urlsafe(32)
                self.id = str(self.session['id'])
-               self.global_dict[self.id] = {
-                                           'assist':[],
-                                           'psy1':[],
-                                           'psy2':[],
-                                           'psy1_ch':[],
-                                           'psy2_ch':[]
-                                           }                 #Определяем словарь под текущую сессию 
+               self.global_dict[self.id] = {                #Определяем словарь под текущую сессию
+                                           'assist':[],     #Данные ассистента
+                                           'psy1':[],       #Первого экстрасенса
+                                           'psy2':[],       #Второго экстрасенса
+                                           'psy1_cr':0,     #Достовернось первого
+                                           'psy2_cr':0      #Достоверность второго 
+                                           }                 
                
-               return "<center>Программа тестирования экстрасенсов <br><a href = '/test_ext/'></b>" + \
-                      "<p><input type = submit value = Приступить к тестированию></p></b></a></center>"\
-                      ''+ '' +''   
+               return "<br><br><br><center><h2>Загадайте двухзначное\
+                      число<br><a href = '/test_ext/'>" + \
+                      "<p><input type = submit value = Загадал>\
+                      </p></a></h2></center>"
+                      
         
         @app.route('/test_ext/', methods = ['GET', 'POST'])
         def test_ext():
             
             name =  str(self.session['id'])
-            
-            self.psy1.guess() #Загадывает первый экстрасенс
-            self.psy2.guess() #Загадывает второй экстрасенс
-                
+                            
             if request.method == 'POST':
-               self.global_dict[name]['assist'].append(request.form.get('num_test'))
-               '''   
-               if not ass:
-                   ass[name] = [(request.form.get('num_test'))]
+                if not request.form.get('num_test'):
                    
-                  # ids[self.psy1.name] = [self.psy1.num_gu]
-                  # ids[self.psy2.name] = [self.psy2.num_gu]
-               else:
-                   ass[name].append(request.form.get('num_test'))
-                  # ids[self.psy1.name].append(self.psy1.num_gu)
-                  # ids[self.psy2.name].append(self.psy2.num_gu)
-                  '''
-               return redirect('/results/')
-                  
-            return'<center>Экстрасенс ' + self.psy1.name +\
-                 ' предсказывает что загаданное число ' + str(self.psy1.num_gu) + '<br>' + \
+                   return"<br><br><br><center><h2>Экстрасенсы сбиваются.\
+                          Пожалуйста не вводите их в заблуждение!<dr>\
+                           <br><a href = '/test_ext/'>" + \
+                          "<p><input type = submit value = Хорошо>\
+                          </p></a></h2></center>"
+                else:
+                   guss_num = int(request.form.get('num_test'))                         #Загаданный номер
+                   self.global_dict[name]['assist'].append(guss_num)                    #Записываем 
+                   self.global_dict[name]['psy1'].append(self.psy1.num_gu)               #данные
+                   self.global_dict[name]['psy2'].append(self.psy2.num_gu)               #
+                   self.global_dict[name]['psy1_cr'] = self.psy1.creds(
+                                                        self.global_dict[name]['psy1_cr'],
+                                                        guss_num,
+                                                        self.psy1.num_gu
+                                                                       )
+                   self.global_dict[name]['psy2_cr'] = self.psy2.creds(
+                                                        self.global_dict[name]['psy2_cr'],
+                                                        guss_num,
+                                                        self.psy2.num_gu
+                                                                       )
+               
+                   return redirect('/results/')
+               
+            self.psy1.guess() #Загадывает первый экстрасенс
+            self.psy2.guess() #Загадывает второй экстрасенс      
+            
+            return'<h2><center>Экстрасенс ' + self.psy1.name +\
+                 ' предсказывает, что загаданное число ' + str(self.psy1.num_gu) + '<br>' + \
                  '<center>Экстрасенс ' + self.psy2.name +\
-                 ' предсказывает что загаданное число ' + str(self.psy2.num_gu) + '<br>' + \
-                 'Введите правильное число!'\
+                 ' предсказывает, что загаданное число ' + str(self.psy2.num_gu) + '<br>' + \
+                 'Введите загаданное Вами число!'\
                  '<form action = "/test_ext/" method = "post" >'\
                  '<p><input type = "number" min = 10 max = 99 name = num_test></p>'\
                  '<p><input type = submit value = Отправить></p>'\
-                 '</form></center>'
+                 '</form></center></h2>'
                  
 
         @app.route('/results/')
@@ -77,12 +90,24 @@ class Assays():
             name =  str(self.session['id'])
              
             lst_assist = self.global_dict[name]['assist']
-           # lst_psy1 =  ids[self.psy1.name] 
-           # lst_psy2 =  ids[self.psy2.name]
+            lst_psy1 = self.global_dict[name]['psy1']
+            lst_psy2 = self.global_dict[name]['psy2']
+            psy1_cr = self.global_dict[name]['psy1_cr']
+            psy2_cr = self.global_dict[name]['psy2_cr']
                  
-            return '<center>Загаданное число '+ str(lst_assist[-1]) +  '<br>' + \
-                   'Первый ' + 'str(lst_psy1[-1])'+ ' Второй ' + 'str(lst_psy2[-1])'+ '<br>'+ \
-                   'Все числа' + str(lst_assist) + '<br>'\
-                   "<b><a href = '/test_ext/'><input type = submit value = Продолжить></a></b></center>"
+            return '<center><h2>Загаданное число '+ str(lst_assist[-1]) +  '<br>' + \
+                   '' + self.psy1.name + ' предположил что это число ' + str(lst_psy1[-1])\
+                   + ', его достоверность составляет   ' + str(psy1_cr) +'.' '<br>' + \
+                   '' + self.psy2.name + ' предположил что это число ' + str(lst_psy2[-1])\
+                    + ', его достоверность составляет   ' + str(psy2_cr) +'.' '' + \
+                    '<br><br><center>Загадайте двухзначное число</h2>'\
+                   "<a href = '/test_ext/'><input type = submit value = Загадал>\
+                   </a></b></center>"'<br>'+ '<br>'+'<br><h3>'+'Числа загаданные помошником'\
+                    + str(lst_assist) + '<br>'\
+                    + 'Числа предложенные экстрасенсом ' + self.psy1.name +' '\
+                     + str(lst_psy1) + '<br>'\
+                     + 'Числа предложенные экстрасенсом ' + self.psy2.name +' '\
+                     + str(lst_psy2) + '</h3><br>'\
+                   
         
 
--- uwsgi/env/psychic.py
+++ uwsgi/env/psychic.py
@@ -1,11 +1,21 @@
 import random
 
 class Psychic():
+    ''' Класс экстрасенс'''
     
     def __init__(self, name):
         self.name = name
         self.num_gu = None
         self.cred = 0
         
-    def guess (self):
+    def guess (self):                            
+        #Догадка
         self.num_gu = random.randint(10,99)
+    
+    def creds (self, res_num, num_assist, num_psych):
+        #Подсчет достоверности
+        if num_assist == num_psych:
+            return res_num + 1
+        else:
+            return res_num - 1
+        
